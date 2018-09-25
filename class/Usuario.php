0766036ab119dab370a1c2b754718cc0daac9a5c
Jeferson Andrade <jeferson.r.andrade@gmail.com>
2018-09-25 14:41:24 -0300

    classe usuario pronta

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,6 +1,6 @@
 <?php
 
-class Ususario {
+class Usuario {
 
 	private $idusuario;
 	private $deslogin;
@@ -40,10 +40,36 @@ class Ususario {
 	}
 
 	public function loadById($id) {
+
 		$sql = new Sql();
 
-		$results = $sql->("SELECT * FROM tb_ususarios WHERE idusuario");
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID",array(
+			":ID"=>$id
+		));
+
+		if (count($results) > 0) {
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		}
 	}
+
+	public function __toString() {
+
+		return json_encode(array(
+			"idusuario"=>$this->getIdusuario(),
+			"deslogin"=>$this->getDeslogin(),
+			"dessenha"=>$this->getDessenha(),
+			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+		));
+
+	}
+
 }
 
 ?>
